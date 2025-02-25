76f09d26c02de5d2db24dffa5fe840cfcd5ceb31
Raül Lama <110611752+Raullam@users.noreply.github.com>
2025-02-25 21:29:45 +0100

    Update plantas.js

--- routes/plantas.js
+++ routes/plantas.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const router = express.Router()
-const db = require('../config/db') // Asegúrate de ajustar la ruta según la ubicación de tu archivo db.js
+const db = require('../config/db')
 
 /**
  * @swagger
